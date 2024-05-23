46ab776aaa8df3f5abc17bb8a1d0cae2e6fc83a4
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-23 15:10:26 +0200

    implementing bonus

--- src/wish/list/Main.java
+++ src/wish/list/Main.java
@@ -1,21 +1,54 @@
 package wish.list;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         //creo una lista
         ArrayList<String> christmasGifts = new ArrayList<>();
 
+        //prendo il file
+        File file = new File("./resources/data.txt");
+
+        //inizializzo gli scanner
         Scanner scanner = new Scanner(System.in);
+        Scanner fileScanner = new Scanner(file);
+
+        //leggo dal file dall'inizio
+        while(fileScanner.hasNextLine()) {
+            //se ho qualosa all'interno del file lo aggiungo alla mia lista
+            String gift = fileScanner.nextLine();
+            christmasGifts.add(gift);
+        }
+
+        if (christmasGifts.isEmpty()) {
+            System.out.println("La tua lista è vuota");
+        } else {
+            //se la lista non è vuota mostro cosa ha
+            System.out.print("La tua lista contiente: ");
+            for (int i = 0; i < christmasGifts.size(); i++) {
+                if (i == christmasGifts.size() - 1) {
+                    System.out.print(christmasGifts.get(i) + ".");
+                } else {
+                    System.out.print(christmasGifts.get(i) + ", ");
+
+                }
+            }
+            System.out.println();
+        }
+
         boolean play = true;
 
-        while(play) {
+        //finchè l'utente non ferma l'app aggiunge regali alla lista
+        while (play) {
 
             System.out.println("Inserisci il regalo che vuoi aggiungere alla lista: ");
             String gift = scanner.nextLine();
@@ -28,9 +61,7 @@ public class Main {
             System.out.println("****************************");
             System.out.println("Vuoi aggiungere un regalo? s/n");
             String choice = scanner.nextLine();
-            if (choice.equalsIgnoreCase("s")) {
-                continue;
-            } else {
+            if (!choice.equalsIgnoreCase("s")) {
                 play = false;
             }
 
@@ -38,23 +69,26 @@ public class Main {
 
         //ordino la lista
         Collections.sort(christmasGifts);
-        //ho optato per fori perchè avevo bisogno dell'index
-//        System.out.println("Questa è la tua lista di regali: ");
-//        for (String gift : christmasGifts) {
-//            System.out.println(gift);
-//        }
+
+
+        //scrivo nel file txt
+
+        FileWriter fileWriter = new FileWriter(file, true);
 
         System.out.print("Questa è la tua lista di regali: ");
         //mostro tutti gli elementi della lista
         for (int i = 0; i < christmasGifts.size(); i++) {
+
+            //scrivo il regalo nel file
+            fileWriter.write(christmasGifts.get(i) + "\n");
             if (i == christmasGifts.size() - 1) {
                 System.out.print((christmasGifts.get(i)) + ".");
             } else {
                 System.out.print(christmasGifts.get(i) + ", ");
             }
         }
-
-
+        fileWriter.close();
         scanner.close();
+
     }
 }
